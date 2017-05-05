055379b3e3da78d35a0170205a68ed72fdfed893
Sarah Sweat <sarahsweat@gmail.com>
2017-05-05 20:54:31 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  
+  i = 1
+  
+  while i <= array.size
+
+    yield(array[i-1])
+    
+    i += 1
+    
+  end
+
+  array 
+  
 end
